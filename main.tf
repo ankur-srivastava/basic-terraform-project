e1d01c90932bdfc3fb622b180a06cf7932b70080
ankur-srivastava <ankurlkw@gmail.com>
2023-01-19 07:11:42 +0530

    refactored

--- main.tf
+++ main.tf
@@ -3,5 +3,12 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "my_s3_bucket" {
-    bucket = "my-bucket-ankurlkw-2"
+    bucket = "my-bucket-ankurlkw-02"
+    versioning {
+        enabled = true
+    }
+}
+
+resource "aws_iam_user" "my_iam_user" {
+    name = "my_iam_user_test1"
 }
---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "my_s3_bucket_versioning" {
+    value = aws_s3_bucket.my_s3_bucket.versioning[0].enabled
+}
+
+output "my_s3_bucket_complete_details" {
+    value = aws_s3_bucket.my_s3_bucket
+}
